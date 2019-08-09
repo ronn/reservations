883fccf1479467c8ae4f875964ec76b2f33e97fc
ronn <ronnie@nextdigital.es>
2019-08-09 23:25:17 +0200

    Add first entity model

--- src/main/java/com/ronn/reservations/domain/model/Client.java
+++ src/main/java/com/ronn/reservations/domain/model/Client.java
@@ -1,4 +1,29 @@
 package com.ronn.reservations.domain.model;
 
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.Set;
+
+@Data
+@Entity
 public class Client {
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
+    private String id;
+    private String dni;
+    private String name;
+    private String lastName;
+    private String adress;
+    private String phoneNumber;
+    private String email;
+
+    @OneToMany(mappedBy = "client")
+    private Set<Reservation> reservations;
 }
--- src/main/java/com/ronn/reservations/domain/model/Reservation.java
+++ src/main/java/com/ronn/reservations/domain/model/Reservation.java
@@ -1,4 +1,26 @@
 package com.ronn.reservations.domain.model;
 
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Data
 public class Reservation {
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
+    private String id;
+
+    private LocalDate startDate;
+    private LocalDate finishDate;
+    private Integer peopleQuantity;
+
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
 }
