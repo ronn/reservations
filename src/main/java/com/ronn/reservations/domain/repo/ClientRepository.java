39444440f299df10c32a19f917f5b82bef065728
ronn <ronnie@nextdigital.es>
2019-08-12 23:36:15 +0200

    Make findClient return an Option

--- src/main/java/com/ronn/reservations/domain/repo/ClientRepository.java
+++ src/main/java/com/ronn/reservations/domain/repo/ClientRepository.java
@@ -3,7 +3,9 @@ package com.ronn.reservations.domain.repo;
 import com.ronn.reservations.domain.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClientRepository extends JpaRepository<Client, String> {
 
-    Client findClientByDni(String dni);
+    Optional<Client> findClientByDni(String dni);
 }
